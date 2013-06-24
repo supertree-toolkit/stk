d14b0f266113c1c60a79649211582be1f6b33a2e
Jon Hill <jon.hill@imperial.ac.uk>
2013-06-24 16:57:43 +0100

    Update to schema validation test to add in stubs. Moved some phyml back to xml stubs

--- stk/test/_schema_validation.py
+++ stk/test/_schema_validation.py
@@ -105,18 +105,36 @@ class SchemaValidator:
   def OptionErrors(self):
     return self._optionErrors
 
-validator = SchemaValidator(rootDir = "data/input")
+validator = SchemaValidator(rootDir = "data")
 
-validator.ValidateOptionsFiles(schemafile = os.path.join("../../../../schema", "phylo_storage.rng"), testDir = ".", depth = 1, extension = "phyml", xmlRootNode = "phylo_storage")
+
 
 class TestSchema(unittest.TestCase):
 
-    def test_validation(self):
+    def test_validation_input_phyml(self):
+        validator.ValidateOptionsFiles(schemafile = os.path.join("../../../schema", "phylo_storage.rng"), testDir = "input", depth = 1, extension = "phyml", xmlRootNode = "phylo_storage")
         passes = validator.Passes()
         optionErrors = validator.OptionErrors()
         self.assert_(len(optionErrors) == 0)
 
+    def test_validation_output_phyml(self):
+        validator.ValidateOptionsFiles(schemafile = os.path.join("../../../schema", "phylo_storage.rng"), testDir = "output", depth = 1, extension = "phyml", xmlRootNode = "phylo_storage")
+        passes = validator.Passes()
+        optionErrors = validator.OptionErrors()
+        self.assert_(len(optionErrors) == 0)
+
+    def test_validation_input_stubs(self):
+        validator.ValidateOptionsFiles(schemafile = os.path.join("../../../schema", "phylo_storage.rng"), testDir = "input", depth = 1, extension = "xml", xmlRootNode = "source")
+        passes = validator.Passes()
+        optionErrors = validator.OptionErrors()
+        self.assert_(len(optionErrors) == 0)
     
+    def test_validation_output_stubs(self):
+        validator.ValidateOptionsFiles(schemafile = os.path.join("../../../schema", "phylo_storage.rng"), testDir = "output", depth = 1, extension = "xml", xmlRootNode = "source")
+        passes = validator.Passes()
+        optionErrors = validator.OptionErrors()
+        self.assert_(len(optionErrors) == 0)
+
 if __name__ == '__main__':
     unittest.main()
 
--- stk/test/_set_source_name.py
+++ stk/test/_set_source_name.py
@@ -9,14 +9,14 @@ from lxml import etree
 from util import *
 
 # our test dataset
-xml_single = etree.parse("data/input/single_name.xml")
-xml_single_c = etree.parse("data/output/single_name.xml")
-xml_two = etree.parse("data/input/two_names.xml")
-xml_two_c = etree.parse("data/output/two_names.xml")
-xml_lots = etree.parse("data/input/lots.xml")
-xml_lots_c = etree.parse("data/output/lots.xml")
-xml_full = etree.parse("data/input/full_tree.xml")
-xml_full_c = etree.parse("data/output/full_tree.xml")
+xml_single = etree.parse("data/input/single_name.phyml")
+xml_single_c = etree.parse("data/output/single_name.phyml")
+xml_two = etree.parse("data/input/two_names.phyml")
+xml_two_c = etree.parse("data/output/two_names.phyml")
+xml_lots = etree.parse("data/input/lots.phyml")
+xml_lots_c = etree.parse("data/output/lots.phyml")
+xml_full = etree.parse("data/input/full_tree.phyml")
+xml_full_c = etree.parse("data/output/full_tree.phyml")
 non_unique_names = etree.parse("data/input/non_unique_names.phyml")
 
 class TestSetSourceNames(unittest.TestCase):
