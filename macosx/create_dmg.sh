14e1e45df9186e9561c4444dff85b3c5e2f086f3
Jon Hill <jon.hill@imperial.ac.uk>
2013-10-22 12:11:15 +0100

    Adding correct scripts to produce a Mac App and DMG

---
+++ macosx/create_dmg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+hdiutil convert stk_template.sparseimage -format UDBZ -o stk_template.dmg
+hdiutil convert stk_template.dmg -format UDSP -o STK
+hdiutil mount STK.sparseimage
+cp -r STK.app /Volumes/STK/
+hdiutil eject /Volumes/STK
+hdiutil convert STK.sparseimage -format UDBZ -o STK.dmg
