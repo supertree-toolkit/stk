34cad918cb74a6c251b02df40cab11ce1fad78a3
jh1889 <jon.hill@york.ac.uk>
2020-05-18 19:59:46 +0100

    Sorting out ; in the output of trim tree to a more suitble character

--- stk/scripts/pipeline/trim_tree.R
+++ stk/scripts/pipeline/trim_tree.R
@@ -5,5 +5,7 @@ library(paleotree)
 tree <- read.tree("dated_tree_randres.tre")
 trim <- 0.13
 chopped<-timeSliceTree(tree, trim, tipLabels="allDesc")
+# replace - with _ in labels
+chopped$tip.label <- gsub(';', '_', chopped$tip.label)
 
 write.tree(chopped,"temp_chopped.tre")
